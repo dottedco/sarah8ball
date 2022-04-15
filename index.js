2faace4a1fb1c8334231d0d1880affbcb405253c
Henry Díaz <61124185+h3nry-d1az@users.noreply.github.com>
2022-04-15 22:21:18 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require('discord.js')
+
+let c8ballcmd = async (message, args, client, _, __) => {
+  const question = args.join(" ")
+  if(!question) return message.reply("`Ask me a question.`")
+
+  let responses = ["Yes", "No", "Maybe", "Ask someone else"]
+  let random = responses[Math.floor(Math.random() * responses.length)];
+
+  const resp = new MessageEmbed()
+  .setTitle("***8ball***")
+  .setDescription(`The response to ${question}\n\n is:\n**${random}**`)
+  .setColor("PURPLE")
+  .setThumbnail(message.guild.iconURL())
+  .setTimestamp()
+  message.channel.send({ embeds: [resp] })
+}
+
+module.exports.c8ballcmd = c8ballcmd
